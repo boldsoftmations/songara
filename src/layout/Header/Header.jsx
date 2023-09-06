e10f7f31af840eec141365b15b95d97b6a3f3a45
vivek-boldSoftMations <vivek@glutape.com>
2023-09-06 11:45:26 +0530

    feat : added sidebar and header

---
+++ src/layout/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './Header.module.css';
+
+const Header = () => {
+  return (
+    <div className={styles.header}>
+      {/* Your header content here */}
+      <h3>Songara International LLP</h3>
+    </div>
+  );
+};
+
+export default Header;
